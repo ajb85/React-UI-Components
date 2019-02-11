ac8e3275123d90393c6760c43f7f11003a65725b
ajb85 <ajbrush85@gmail.com>
2019-02-11 17:35:16 -0500

    Clicking card content now redirects to react docs.  MVP officially reached!

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -6,9 +6,13 @@ import Content from "./CardContent.js";
 
 export default function() {
   return (
-    <div>
+    <div className="cardContainer" onClick={handleClick}>
       <Header />
       <Content />
     </div>
   );
+}
+
+function handleClick(e) {
+  window.location.href = "https://www.reactjs.org";
 }
