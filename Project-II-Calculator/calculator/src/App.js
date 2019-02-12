35bc4aa856902e07cba04a2c28b57e67b943ff93
ajb85 <ajbrush85@gmail.com>
2019-02-12 16:04:37 -0500

    Basic calculator layout + styling.  Zero functionality

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -1,23 +1,105 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./css/index.css";
+import Button from "./components/Buttons/Button.js";
+const buildCalc = [
+  {
+    rowCount: 4,
+    class: "display",
+    children: 0
+  },
+  {
+    rowCount: 3,
+    class: "button clear",
+    children: "clear"
+  },
+  {
+    rowCount: 1,
+    class: "button operator",
+    children: "÷"
+  },
+  {
+    rowCount: 1,
+    class: "button num",
+    children: "7"
+  },
+  {
+    rowCount: 1,
+    class: "button num",
+    children: "8"
+  },
+  {
+    rowCount: 1,
+    class: "button num",
+    children: "9"
+  },
+  {
+    rowCount: 1,
+    class: "button operator",
+    children: "x"
+  },
+  {
+    rowCount: 1,
+    class: "button num",
+    children: "4"
+  },
+  {
+    rowCount: 1,
+    class: "button num",
+    children: "5"
+  },
+  {
+    rowCount: 1,
+    class: "button num",
+    children: "6"
+  },
+  {
+    rowCount: 1,
+    class: "button operator",
+    children: "-"
+  },
+  {
+    rowCount: 1,
+    class: "button num",
+    children: "1"
+  },
+  {
+    rowCount: 1,
+    class: "button num",
+    children: "2"
+  },
+  {
+    rowCount: 1,
+    class: "button num",
+    children: "3"
+  },
+  {
+    rowCount: 1,
+    class: "button operator",
+    children: "+"
+  },
+  {
+    rowCount: 3,
+    class: "button num",
+    children: "0"
+  },
+  {
+    rowCount: 1,
+    class: "button operator",
+    children: "="
+  }
+];
 
 const App = () => {
-  return (
-    <div>
-      <h3>Welcome to React Calculator</h3>
-      <p>
-        We have given you a starter project. You'll want to build out your
-        components in their respective files, remove this code and replace it
-        with the proper components.
-      </p>
-      <p>
-        <strong>
-          Don't forget to `default export` your components and import them here
-          inside of this file in order to make them work.
-        </strong>
-      </p>
-    </div>
-  );
+  const buttons = buildCalc.map(button => {
+    return (
+      <Button
+        row={button.rowCount}
+        className={button.class}
+        children={button.children}
+      />
+    );
+  });
+  return <div className="container calc">{buttons}</div>;
 };
 
 export default App;
